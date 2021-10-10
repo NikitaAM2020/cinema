386a001de18f51ad5a44b994849c9ca862b6077a
Nikita Zhuravel <90179715+NikitaAM2020@users.noreply.github.com>
2021-10-10 09:33:28 +0300

    new

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const schemeSvg = document.querySelector('.scheme-svg');
+const totalPriseTag = document.querySelector('.price-total');
+let cost = 120;
+let totalPrise = 0;
+schemeSvg.addEventListener('click', (event) => {
+    if (!event.target.classList.contains('booked')){
+        event.target.classList.toggle('active');
+        let totalSeats = schemeSvg.querySelectorAll('.active').length;
+        totalPrise = totalSeats * cost;
+        totalPriseTag.textContent = totalPrise;
+        
+    }     
+});
