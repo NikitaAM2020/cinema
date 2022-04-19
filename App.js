ef9dae06f09bc9cd6e151ca2198e351ad4e1cc03
NikitaAM2020 <nikita.zhuravel24@gmail.com>
2022-04-19 11:45:14 +0300

    Add App

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './components/nav/Nav'
+import Header from './components/header/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <Header />
+    </div>
+    
+  )
+}
+
+export default App
